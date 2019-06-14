55c07e30dbc25c16a96e94d19aed43d9c9c6de1b
Sydney Noh <sydneycnoh@gmail.com>
2019-06-14 15:25:33 -0500

    Daily Journal 5 content

---
+++ src/scripts/entriesDOM.js
@@ -0,0 +1,12 @@
+const journalTextDiv = document.querySelector("#entry_log")
+
+
+
+const renderJournalEntries = (entries) => {
+    console.log("entries", entries);
+    for(i = 0; i < entries.length; i++) {
+        let finishedDiv = makeJournalEntryComponent(entries[i]); // Passes in each object in array objectJournalEntry
+        // console.log("finished div", finishedDiv)
+        journalTextDiv.innerHTML += finishedDiv // Concats values to create finished #entry_log HTML
+    }
+}
---
+++ src/scripts/entryComponent.js
@@ -0,0 +1,11 @@
+const makeJournalEntryComponent = (journalEntry) => {
+    return `
+    <div>
+        <h1>Journal Entry</h1>
+        <h3>Date: ${journalEntry.date}</h3>
+        <h3>Concepts: ${journalEntry.concepts}</h3>
+        <p>Journal Entry: ${journalEntry.journal_entry}</p>
+        <p>Mood: ${journalEntry.mood}</p>
+    </div>
+    `
+}
