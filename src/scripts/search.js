6bb547cbf24f66627f633685007d7bdfadf0e46b
Sydney Noh <sydneycnoh@gmail.com>
2019-06-27 13:26:13 -0500

    Added stashed stuff from previosu branch

---
+++ src/scripts/search.js
@@ -0,0 +1,13 @@
+/* TODO:
+-create function to take each radio button and add click event
+-need to create one click event that when selected it will grab the selected name's value
+-then it will need to use a function that filters through each one of the objects and -filter the selected mood and put it into a new array
+-append it to the DOM */
+
+let radioButtons = document.getElementsByName("mood")
+
+console.log(radioButtons)
+
+radiobutton.forEach((node)=> {
+    
+})
